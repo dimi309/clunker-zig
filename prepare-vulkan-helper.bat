fb6d73db43b5accaa261be0f2f3aabf9756cbb5d
dimi309 <dimi309@users.noreply.github.com>
2023-03-31 20:09:05 +0200

    Initial commit

---
+++ prepare-vulkan-helper.bat
@@ -0,0 +1,17 @@
+git submodule update --init
+cd vulkan_helper\resources\shaders
+glslangValidator -V fragmentShader.frag -o fragmentShader.spv
+glslangValidator -V vertexShader.vert -o vertexShader.spv
+cd ..\..
+mkdir build
+cd build
+cmake .. -G"Visual Studio 17 2022"
+cmake --build . --config Debug
+
+xcopy include ..\..\include /i /s /y
+xcopy lib ..\..\lib /i /s /y
+cd ..
+xcopy resources ..\resources /i /s /y
+mkdir ..\zig-out\bin
+xcopy resources ..\zig-out\bin\resources /i /s /y
+cd ..
